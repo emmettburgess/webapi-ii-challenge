0a493452c5291717603e751870edbff4a278398e
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-18 21:05:15 -0600

    index

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./server.js');
+
+server.listen(9090, () => {
+  console.log('\n*** Server Running on http://localhost:9090 ***\n');
+});
+
+const server= require('./server.js');
